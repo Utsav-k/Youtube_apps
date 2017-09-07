029066894e54bae9d4b8b382b5c14b09468e5f79
Utsav Kaushik <utsav.kaushik.77@gmail.com>
2017-09-08 04:03:17 +0530

    Created video_player.py
    This script will open searched video (which is the first video result of youtube search engine) in the default browser, also save your search results as video url in a text file for further usage.

---
+++ video_player.py
@@ -0,0 +1,56 @@
+import os
+import random
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
+
+query = input("Enter song name: ")
+mystring = query.replace(' ' ,'+')
+
+pref = "https://www.youtube.com/results?search_query="
+url = pref+mystring
+
+print("Directing to the url, Please wait!")
+
+user_agent = {'User-agent': 'Mozilla/5.0'}
+r = requests.get(url, headers = user_agent)
+
+if r.status_code == 200:
+	print("Request Approved!")
+
+html_doc = r.text
+soup = BeautifulSoup(html_doc, 'html.parser')
+soup.prettify()
+
+result = str()
+
+for link in soup.find_all('a'):
+	a = link.get('href')
+	if 'watch' in a:
+		result = a
+		break
+
+new_url = 'https://www.youtube.com'+result
+
+webbrowser.open_new_tab(new_url)
+
+print("Hold on! It's your browser taking time.....")
+
+
+saveFile =  open('songs.txt')
+
+lines = saveFile.readlines()
+lines = [x.strip() for x in lines]
+
+text = new_url
+ctr = True
+for line in lines:
+	if text == line:
+		ctr = False
+		break
+
+if ctr == True:
+	saveFile = open('songs.txt','a')
+	saveFile.write('\n'+text)
+
+saveFile.close()
